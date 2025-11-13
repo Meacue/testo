683ac2d420f2bfefee983f5e30e1263a295dd340
petrdobr <dobrokhotovp@gmail.com>
2025-11-13 17:21:15 +0100

    feat(assert): add `NumericTrait` to store numeric assertions

--- src/Assert/DataType/AssertInt.php
+++ src/Assert/DataType/AssertInt.php
@@ -4,61 +4,16 @@ declare(strict_types=1);
 
 namespace Testo\Assert\DataType;
 
-use Testo\Assert\State\AssertException;
-use Testo\Assert\StaticState;
+use Testo\Assert\Traits\NumericTrait;
 
 /**
  * Assertion utilities for integer data type.
  */
 class AssertInt
 {
+    use NumericTrait;
+
     public function __construct(
         public int $value,
     ) {}
-
-    /**
-     * Asserts that the integer value is greater than given value.
-     *
-     * @param int $min Minimum threshold to compare with.
-     * @param string $message Optional message for the assertion.
-     * @throws AssertException when the assertion fails.
-     */
-    public function greaterThan(int $min, string $message = ''): void
-    {
-        if ($this->value > $min) {
-            StaticState::log('Assert int greater than `' . $min . '`', $message);
-            return;
-        }
-
-        StaticState::fail(AssertException::compare(
-            $min,
-            $this->value,
-            $message,
-            pattern: 'Failed asserting that value `%2$s` is greater than `%1$s`.',
-            showDiff: false,
-        ));
-    }
-
-    /**
-     * Asserts that the integer value is greater than or equal to given value.
-     *
-     * @param int $min Minimum threshold to compare with.
-     * @param string $message Optional message for the assertion.
-     * @throws AssertException when the assertion fails.
-     */
-    public function greaterThanOrEqual(int $min, string $message = ''): void
-    {
-        if ($this->value >= $min) {
-            StaticState::log('Assert int greater than or equal to `' . $min . '`', $message);
-            return;
-        }
-
-        StaticState::fail(AssertException::compare(
-            $min,
-            $this->value,
-            $message,
-            pattern: 'Failed asserting that value `%2$s` is greater than or equal to `%1$s`.',
-            showDiff: false,
-        ));
-    }
 }
