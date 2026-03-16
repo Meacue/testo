27fefa8ba6d28dda197d16a7b7ef37c7dff4069f
roxblnfk <roxblnfk@ya.ru>
2026-03-16 17:24:14 +0400

    refactor!: Remove `$parallel` flag from `SuiteConfig`

--- src/Application/Config/SuiteConfig.php
+++ src/Application/Config/SuiteConfig.php
@@ -22,8 +22,6 @@ final readonly class SuiteConfig
      * @param non-empty-string $name A unique name for the test suite.
      * @param FinderConfig|list<non-empty-string|Path> $location Configuration for locating test cases.
      *        An array is a shortcut for `new FinderConfig(include: $location)`.
-     * @param bool $parallel Whether tests in this suite should be executed in parallel.
-     *        Note: Parallel execution is not supported yet and will be ignored.
      * @param iterable<PluginConfigurator> $plugins List of plugins to load for this suite.
      *        An array of plugin instances is treated as {@see SuitePlugins::with()}.
      *        Use {@see SuitePlugins} facade for advanced configuration.
@@ -31,7 +29,6 @@ final readonly class SuiteConfig
     public function __construct(
         public string $name,
         FinderConfig|array $location,
-        public bool $parallel = false,
         public iterable $plugins = [],
     ) {
         $this->location = $location instanceof FinderConfig
@@ -41,13 +38,11 @@ final readonly class SuiteConfig
 
     public function with(
         ?FinderConfig $location = null,
-        ?bool $parallel = null,
         iterable|null $plugins = null,
     ): self {
         return new self(
             name: $this->name,
             location: $location ?? $this->location,
-            parallel: $parallel ?? $this->parallel,
             plugins: $plugins ?? $this->plugins,
         );
     }
